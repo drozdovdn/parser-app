1b9e4f421947c18071d5079f2b6678c7b5b2e8ae
drozdovdn <drozdovdn@alfastrah.ru>
2025-05-17 20:46:22 +0300

    fix api and code

--- backend/src/errors/forbidden-error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ForbiddenError extends Error {
-    constructor(message) {
-        super(message);
-        this.statusCode = 403;
-    }
-}
-exports.default = ForbiddenError;
--- backend/src/errors/unauthorized-error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UnauthorizedError extends Error {
-    constructor(message) {
-        super(message);
-        this.statusCode = 401;
-    }
-}
-exports.default = UnauthorizedError;
